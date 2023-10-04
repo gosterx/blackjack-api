5906d9b5621894bcfc4d1308862f8dd02a77aaf1
Aliaksei Suslau <aliaksei.suslau02@gmail.com>
2023-10-04 18:15:09 +0200

    Add initial dependencies

--- build.sbt
+++ build.sbt
@@ -1,8 +1,26 @@
+import Dependencies._
+
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.1"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "blackjack-api"
+    name := "blackjack-api",
+    libraryDependencies ++= Seq(
+      Cats.core,
+      Cats.effect,
+      Http4s.dsl,
+      Http4s.server,
+      Http4s.circe,
+      Circe.core,
+      Circe.parser,
+      Circe.refined,
+      Doobie.postgres,
+      Refined.core,
+      Refined.cats,
+      log4CatsNoop,
+      flyway,
+      redis4catsEffects
+    )
   )
---
+++ project/Dependencies.scala
@@ -0,0 +1,33 @@
+import sbt._
+
+object Dependencies {
+  object Cats {
+    val core       = "org.typelevel" %% "cats-core"   % "2.10.0"
+    val effect = "org.typelevel" %% "cats-effect" % "3.5.2"
+  }
+
+  object Circe {
+    val core    = "io.circe" %% "circe-core"    % "0.14.6"
+    val parser  = "io.circe" %% "circe-parser"  % "0.14.6"
+    val refined = "io.circe" %% "circe-refined" % "0.14.6"
+  }
+
+  object Http4s {
+    val dsl    = "org.http4s" %% s"http4s-dsl"          % "1.0.0-M40"
+    val server = "org.http4s" %% s"http4s-ember-server" % "1.0.0-M40"
+    val circe  = "org.http4s" %% s"http4s-circe"        % "1.0.0-M40"
+  }
+
+  object Doobie {
+    val postgres = "org.tpolecat" %% "doobie-postgres" % "1.0.0-RC4"
+  }
+
+  object Refined {
+    val core = "eu.timepit" %% "refined"      % "0.11.0"
+    val cats = "eu.timepit" %% "refined-cats" % "0.11.0"
+  }
+
+  val log4CatsNoop      = "org.typelevel"  %% "log4cats-noop"      % "2.6.0"
+  val flyway            = "org.flywaydb"    % "flyway-core"        % "8.5.13"
+  val redis4catsEffects = "dev.profunktor" %% "redis4cats-effects" % "1.5.0"
+}
